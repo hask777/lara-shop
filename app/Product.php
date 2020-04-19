2f483467788ad3252e904e2ae0e641ed7aadcf91
hask777 <haskellisp@gmail.com>
2020-04-19 21:03:20 +0400

    add categories to db

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    public function categories(){
+        return $this->belongsToMany('App\Category');
+    }
+
     public function scopeRelative($query){
         return $query->inRandomOrder()->take(4);
     }
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -20,8 +20,11 @@ class ProductsTableSeeder extends Seeder
                 'details' => [13,14,15][array_rand([13,14,15])] . ' inch, ' . [1, 2, 3][array_rand([1, 2, 3])] .' TB SSD, 32GB RAM',
                 'price' => rand(149999, 249999),
                 'description' =>'Lorem '. $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(1);
         }
+
+        $product = Product::find(1);
+        $product->categories()->attach(2);
 
 
         // Desktops
@@ -32,7 +35,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => [24, 25, 27][array_rand([24, 25, 27])] . ' inch, ' . [1, 2, 3][array_rand([1, 2, 3])] . ' TB SSD, 32GB RAM',
                 'price' => rand(249999, 449999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(2);
         }
 
         // Phones
@@ -43,7 +46,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [7, 8, 9][array_rand([7, 8, 9])] . ' inch screen, 4GHz Quad Core',
                 'price' => rand(79999, 149999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(3);
         }
 
         // Tablets
@@ -54,7 +57,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => [16, 32, 64][array_rand([16, 32, 64])] . 'GB, 5.' . [10, 11, 12][array_rand([10, 11, 12])] . ' inch screen, 4GHz Quad Core',
                 'price' => rand(49999, 149999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(4);
         }
 
         // TVs
@@ -65,7 +68,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => [46, 50, 60][array_rand([7, 8, 9])] . ' inch screen, Smart TV, 4K',
                 'price' => rand(79999, 149999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(5);
         }
 
         // Cameras
@@ -76,7 +79,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => 'Full Frame DSLR, with 18-55mm kit lens.',
                 'price' => rand(79999, 249999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(6);
         }
 
         // Appliances
@@ -87,7 +90,7 @@ class ProductsTableSeeder extends Seeder
                 'details' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, dolorum!',
                 'price' => rand(79999, 149999),
                 'description' => 'Lorem ' . $i . ' ipsum dolor sit amet, consectetur adipisicing elit. Ipsum temporibus iusto ipsa, asperiores voluptas unde aspernatur praesentium in? Aliquam, dolore!',
-            ]);
+            ])->categories()->attach(7);
         }
 
     }
